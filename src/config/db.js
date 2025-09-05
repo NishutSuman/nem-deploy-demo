9fa93778b80e8bc9fbb0f8218e0febc7a3be5151
nishutsuman <nishutsuman1998@gmail.com>
2025-09-05 12:14:37 +0530

    Mongo URL Secured

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect("mongodb+srv://nishutsuman1998:Nem123@nemcluster.w4wbzvk.mongodb.net/nemtestdb");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error);
